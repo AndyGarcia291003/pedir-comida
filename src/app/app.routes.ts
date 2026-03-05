048e9775098fef7ed0c57204e59f72bdbed69a95
Andy Ibrahim Garcia Rivera <agarciar081@alumno.uaemex.mx>
2026-03-04 23:06:09 -0600

    feat: implementar componente Home y manejo de rutas validado en localhost

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Perfil } from './pages/perfil/perfil';
 import { Carrito } from './pages/carrito/carrito';
 import { Atriculo } from './pages/atriculo/atriculo';
 import { Rubro } from './pages/rubro/rubro';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   { path: 'buscar', component: Buscar },
@@ -13,6 +14,8 @@ export const routes: Routes = [
   { path: 'carrito', component: Carrito },
   { path: 'atriculo', component: Atriculo },
   { path: 'rubro', component: Rubro },
+  // Ruta comodín: Si el usuario escribe cualquier cosa loca, lo manda a Home
+  { path: '**', component: Home},
   
   // Ruta por defecto (cuando la URL esté vacía)
   { path: '', redirectTo: '/buscar', pathMatch: 'full' }
